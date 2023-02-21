9e106f8bd09fed17af039a1bb712f77e64fee307
Emmanuel Dominion Agbokah <emmanueldominion68@gmail.com>
2023-02-21 11:55:23 -0800

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -28,17 +28,22 @@ void jack_bauer(void)
 			m2 = 0;
 			while (m2 <= 5)
 			{
-				_putchar('0' + h2);
-				_putchar('0' + h1);
-				_putchar(':');
-				_putchar('0' + m2);
-				_putchar('0' + m1);
-				_putchar('\n');
-				m1++;
+				m1 = 0;
+				while (m1 <= 9)
+				{					
+				
+					_putchar('0' + h2);
+					_putchar('0' + h1);
+					_putchar(':');
+					_putchar('0' + m2);
+					_putchar('0' + m1);
+					_putchar('\n');
+					m1++;
+				}
+				m2++;
 			}
-			m2++;
+			h1++;
 		}
-		h1++;
+		h2++;
 	}
-	h2++;
 }
