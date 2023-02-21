8ee39feddbadb8d4223ae59e1a61f856f3e31ad1
Emmanuel Dominion Agbokah <emmanueldominion68@gmail.com>
2023-02-21 08:50:42 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet - function that prints the alphabets in lower case
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet(void)
+
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+
+	_putchar('\n')
+
+}
