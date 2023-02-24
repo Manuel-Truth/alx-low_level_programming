2aa5683d968246df989b1978fd931f38e4f39cca
Emmanuel Dominion Agbokah <emmanueldominion68@gmail.com>
2023-02-23 17:25:02 -0800

    0-main.c

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * positive_or_negative - check if given number is +, - or 0
+ *
+ * @i: number to be checked
+ * Return: nothing on success
+ */
+void positive_or_negative(int i)
+{
+	if (i > 0)
+		printf("%d is postive\n", i);
+	else if (i < 0)
+		printf("%d is negative\n", i);
+	else
+		printf("%d is zero\n", i);
+}
